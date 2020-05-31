8e43e89c49a12cb75f32f89584eaa97bdf54a09b
tino queija <martin.queija@wolox.com.ar>
2020-05-31 13:41:12 -0300

    feat(bootstrap): continue bootstrapping, add personal banner and set my name

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 
-import A from './A';
 import Img from './Img';
-import NavBar from './NavBar';
-import HeaderLink from './HeaderLink';
-import Banner from './banner.jpg';
-import messages from './messages';
+import Banner from './banner.png';
 
 function Header() {
   return (
     <div>
-      <A href="https://www.reactboilerplate.com/">
-        <Img src={Banner} alt="react-boilerplate - Logo" />
-      </A>
-      <NavBar>
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
-        </HeaderLink>
-      </NavBar>
+      <Img src={Banner} alt="hash linked logger banner" />
     </div>
   );
 }
