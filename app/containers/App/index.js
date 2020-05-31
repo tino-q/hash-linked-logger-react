b8015881185a682d84593b9e66b7f3b1b3cf53dd
tino queija <martin.queija@wolox.com.ar>
2020-05-31 13:15:33 -0300

    fix(bootstrap): rename app in helmet and remove feature page and its messages.

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,7 +12,6 @@ import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
@@ -32,15 +31,17 @@ export default function App() {
   return (
     <AppWrapper>
       <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
+        titleTemplate="%s - Hash Linked Logger"
+        defaultTitle="Hash Linked Logger"
       >
-        <meta name="description" content="A React.js Boilerplate application" />
+        <meta
+          name="description"
+          content="Collaborative hash linked logger application"
+        />
       </Helmet>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <Footer />
